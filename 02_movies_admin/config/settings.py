611fde2a2aa251757da79b2b742243fae1c80e36
Sergey <originalustinov@gmail.com>
2022-03-30 00:37:13 +0300

    Move Database configuration to separate file

--- 02_movies_admin/config/settings.py
+++ 02_movies_admin/config/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+from split_settings.tools import include
 
 load_dotenv()
 
@@ -78,21 +78,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', 5432),
-        'OPTIONS': {
-            # Add our 'content' schem
-           'options': '-c search_path=public,content'
-        }
-    }
-}
-
+include(
+    'components/database.py',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
