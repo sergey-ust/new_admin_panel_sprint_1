b258b7129c007387ba7d011b3b14db8caa092497
Sergey <originalustinov@gmail.com>
2022-03-30 02:29:03 +0300

    add genre model to check admin panel

--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -1,1 +1,16 @@
+import uuid
 from django.db import models
+
+
+class Genre(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField('name', max_length=200)
+    description = models.TextField('description', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'content\".\"genre'
+        verbose_name = 'genre'
+        verbose_name_plural = 'genres'
