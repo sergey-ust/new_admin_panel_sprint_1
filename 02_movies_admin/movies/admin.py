7b51e4d9a7bbcb359f18b839c9047523ed68498b
Sergei Ustinov <uss@aerom.ax>
2022-03-30 18:29:52 +0300

    add FilmWork model

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Genre
+from .models import Genre, FilmWork
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
     pass
--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -14,3 +14,20 @@ class Genre(models.Model):
         db_table = 'content\".\"genre'
         verbose_name = 'genre'
         verbose_name_plural = 'genres'
+
+
+class FilmWork(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField('name', max_length=200)
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateField()
+    rating = models.FloatField()
+    type = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'content\".\"film_work'
+        verbose_name = 'film'
+        verbose_name_plural = 'films'
