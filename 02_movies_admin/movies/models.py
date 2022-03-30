3b3f103c5460470dfee05787659184900fab1776
Sergey <originalustinov@gmail.com>
2022-03-30 23:07:21 +0300

    Base classes for autofilled common fields

--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -3,13 +3,24 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Genre(models.Model):
+class TimeStampedMixin(models.Model):
+    created = models.DateTimeField('created', auto_now_add=True)
+    modified = models.DateTimeField('modified', auto_now=True)
 
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(UUIDMixin, TimeStampedMixin):
     name = models.CharField('name', max_length=100)
     description = models.TextField('description', blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'content\".\"genre'
@@ -17,16 +28,14 @@ class Genre(models.Model):
         verbose_name_plural = 'genres'
 
 
-class FilmWork(models.Model):
 
+class FilmWork(UUIDMixin, TimeStampedMixin):
     class FilmType(models.TextChoices):
         MOVIE = 'movie'
         TV_SHOW = 'tv_show'
 
     title = models.CharField('name', max_length=200)
     description = models.TextField('description', blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
     creation_date = models.DateField('creation date')
     rating = models.FloatField(
         'rating',
