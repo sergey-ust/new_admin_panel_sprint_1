682793ed5dcc1fc414c2b6f99e65567a89097427
Sergey <originalustinov@gmail.com>
2022-03-30 23:04:00 +0300

    Correct FilmWork model fields constraint

--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Genre(models.Model):
@@ -18,15 +19,28 @@ class Genre(models.Model):
 
 class FilmWork(models.Model):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    class FilmType(models.TextChoices):
+        MOVIE = 'movie'
+        TV_SHOW = 'tv_show'
+
     title = models.CharField('name', max_length=200)
     description = models.TextField('description', blank=True)
-    creation_date = models.DateField()
-    rating = models.FloatField()
-    type = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    creation_date = models.DateField('creation date')
+    rating = models.FloatField(
+        'rating',
+        blank=True,
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+    )
+    type = models.CharField(
+        "type",
+        max_length=10,
+        choices=FilmType.choices,
+        default=FilmType.MOVIE,
+    )
 
+    # If DB Constraints are needed, they could be added in Meta
     class Meta:
         db_table = 'content\".\"film_work'
         verbose_name = 'film'
