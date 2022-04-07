c126d58d0daaf83baab7219d760ee45b36d71262
Sergey <originalustinov@gmail.com>
2022-04-08 00:48:40 +0300

    add table checking test

--- 03_sqlite_to_postgres/tests/check_consistency.py
+++ 03_sqlite_to_postgres/tests/check_consistency.py
@@ -1,10 +1,15 @@
 import os
 import sqlite3
+from dataclasses import dataclass
+from typing import Callable
 
 from dotenv import load_dotenv
 import pytest
 import psycopg2
 from psycopg2.extras import DictCursor
+
+import load_data
+import tables
 
 
 @pytest.fixture(scope='module')
@@ -60,3 +65,111 @@ def test_sizes(psql_connect, sqlite_connect):
     )
     for tbl in tables_:
         assert get_size(pg_curs, tbl[0]) == get_size(sqlite_curs, tbl[1])
+
+
+@dataclass(frozen=True)
+class Convertor:
+    table_name: str
+    create_class: Callable[[dict], load_data.AnyTable]
+
+
+@dataclass(frozen=True)
+class MainConvertor:
+    postgresql: Convertor
+    sqlite: Convertor
+
+
+def _get_convertors() -> list[MainConvertor]:
+    return [
+        MainConvertor(
+            sqlite=Convertor(
+                table_name='film_work',
+                create_class=lambda d: tables.FilmWork.create_from_sqlite(
+                    tables.SqliteTables.FilmWork(**d)
+                ),
+            ),
+            postgresql=Convertor(
+                table_name='content.film_work',
+                create_class=lambda d: tables.FilmWork(**d),
+            ),
+        ),
+        MainConvertor(
+            sqlite=Convertor(
+                table_name='genre',
+                create_class=lambda d: tables.Genre.create_from_sqlite(
+                    tables.SqliteTables.Genre(**d)
+                ),
+            ),
+            postgresql=Convertor(
+                table_name='content.genre',
+                create_class=lambda d: tables.Genre(**d),
+            ),
+        ),
+        MainConvertor(
+            sqlite=Convertor(
+                table_name='person',
+                create_class=lambda d: tables.Person.create_from_sqlite(
+                    tables.SqliteTables.Person(**d)
+                ),
+            ),
+            postgresql=Convertor(
+                table_name='content.person',
+                create_class=lambda d: tables.Person(**d),
+            ),
+        ),
+        MainConvertor(
+            sqlite=Convertor(
+                table_name='genre_film_work',
+                create_class=lambda d: tables.GenreFilmWork.create_from_sqlite(
+                    tables.SqliteTables.GenreFilmWork(**d)
+                ),
+            ),
+            postgresql=Convertor(
+                table_name='content.genre_film_work',
+                create_class=lambda d: tables.GenreFilmWork(**d),
+            ),
+        ),
+        MainConvertor(
+            sqlite=Convertor(
+                table_name='person_film_work',
+                create_class=lambda d:
+                tables.PersonFilmWork.create_from_sqlite(
+                    tables.SqliteTables.PersonFilmWork(**d)
+                ),
+            ),
+            postgresql=Convertor(
+                table_name='content.person_film_work',
+                create_class=lambda d: tables.PersonFilmWork(**d),
+            ),
+        ),
+    ]
+
+
+_LINES_PER_TIME_EXTRACTION = 400
+
+
+def test_equality(psql_connect, sqlite_connect):
+    sqlite_curs = sqlite_connect
+    pg_curs = psql_connect
+
+    for conv in _get_convertors():
+        offset = 0
+        size = _LINES_PER_TIME_EXTRACTION
+        while True:
+            lite_data = load_data.extract_part(sqlite_curs,
+                                               conv.sqlite.table_name,
+                                               size,
+                                               offset)
+            post_data = load_data.extract_part(pg_curs,
+                                               conv.postgresql.table_name,
+                                               size,
+                                               offset)
+            cnt = len(lite_data)
+            for i in range(cnt):
+                assert conv.postgresql.create_class(dict(post_data[i])) \
+                       == conv.sqlite.create_class(dict(lite_data[i]))
+
+            offset += cnt
+            if cnt < size:
+                print(f'\nLines {offset}')
+                break
